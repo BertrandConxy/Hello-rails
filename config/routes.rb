d4daed21342170d352ce16e9dd0a8f26b8a8212f
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-02 16:58:10 +0200

    add routes and html for the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to:'main#index'
+  get 'pages/hello'
 end
